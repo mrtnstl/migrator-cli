5bf901f80f88785b18f35b221177c27e6017fc4c
Márton Spiteller <spitellermarton@gmail.com>
2026-04-07 18:40:10 +0200

    add: app entrypoint, inquirer and ora package

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { renderMainView } from "./ui/mainView.js";
+
+renderMainView();
+
+/*
+const mySpinner = new Spinner("spinnin'");
+mySpinner.start();
+
+const spinnerTo = setTimeout(() => {
+    mySpinner.text("spinnin' done for good");
+    mySpinner.succeed();
+    clearTimeout(spinnerTo);
+}, 2000);
+*/
+//process.exit();
