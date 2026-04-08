a646138963ded80f7cabc780cec4694773d63849
Márton Spiteller <spitellermarton@gmail.com>
2026-04-08 15:00:20 +0200

    cleanup: change console.log to stdout

--- src/ui/header.ts
+++ src/ui/header.ts
@@ -1,18 +1,14 @@
+import { stdout } from "node:process";
 import { Colors } from "../common/colors.js";
 import { titleMigratorDL } from "./title.js";
 
 export function renderHeader() {
-    console.log(Colors.setColor(titleMigratorDL, { bolds: "green" }));
-    console.log(
+    stdout.write(Colors.setColor(titleMigratorDL, { bolds: "green" }));
+    stdout.write(
         Colors.setColor(
             "=>=>=>=>=>=>=>=>=>=> Migrator v0.0.1 - Centralized database migration handler =>=>=>=>=>=>=>=>=>=>=>",
             { bolds: "white" }
         )
     );
-    console.log(
-        Colors.setColor("test text123", {
-            bolds: "white",
-        })
-    );
-    console.log("");
+    stdout.write("\n");
 }
