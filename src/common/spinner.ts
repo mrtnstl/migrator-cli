8b1967bd01d86ae305bbc39aef4cc7fad159947d
Márton Spiteller <spitellermarton@gmail.com>
2026-04-16 17:24:50 +0200

    add: own spinner implementation to cut down on external dependencies

--- src/common/spinner.ts
+++ src/common/spinner.ts
@@ -1,23 +1,60 @@
-import ora, { Color, Options, Ora } from "ora";
+const colorsLite = {
+    reset: "\x1b[0m",
+    boldBlue: "\x1b[1;34m",
+};
 
 export class Spinner {
-    private instance: Ora;
-    constructor(arg: string | Options) {
-        this.instance = ora(arg);
+    private static readonly frames = ["|", "/", "-", "\\"] as const;
+    private interval: NodeJS.Timeout | null;
+    private message: string;
+    private isRunning: boolean;
+    private currentFrame: number;
+
+    constructor(initialMessage: string) {
+        this.interval = null;
+        this.message = initialMessage;
+        this.isRunning = false;
+        this.currentFrame = 0;
     }
-    start() {
-        this.instance.start();
+
+    start(message?: string) {
+        if (this.isRunning) {
+            return;
+        }
+
+        this.message = message || this.message;
+        this.currentFrame = 0;
+        this.isRunning = true;
+
+        process.stdout.write("\r");
+
+        this.interval = setInterval(() => {
+            const frame = Spinner.frames[this.currentFrame];
+            process.stdout.write(
+                `\r${colorsLite.boldBlue}${frame}${colorsLite.reset} ${this.message}`
+            );
+            this.currentFrame = (this.currentFrame + 1) % Spinner.frames.length;
+        }, 80);
     }
-    stop() {
-        this.instance.stop();
+
+    stop(message: string) {
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+
+        this.isRunning = false;
+
+        process.stdout.write(
+            "\r" + " ".repeat(process.stdout.columns || 80) + "\r"
+        );
+
+        if (message) {
+            console.log(message);
+        }
     }
-    succeed() {
-        this.instance.succeed();
-    }
-    text(text: string) {
-        this.instance.text = text;
-    }
-    color(text: Color) {
-        this.instance.color = text;
+
+    setMessage(newMessage: string) {
+        this.message = newMessage;
     }
 }
