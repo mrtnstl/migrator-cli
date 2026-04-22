b5b721f1404a4f130d12cc71d3fdf9835b68755b
Márton Spiteller <spitellermarton@gmail.com>
2026-04-22 17:09:32 +0200

    fix: linting error in async tests

--- src/tests/unit/internals/runner.test.ts
+++ src/tests/unit/internals/runner.test.ts
@@ -16,26 +16,28 @@ describe("runMigration", () => {
             ],
         ]);
     });
-    /*
-    it("should return void, in case of successful execution", () => {
-        expect(() => {
-            runMigration(0, "up");
-        }).toBe(undefined);
-    });
-    */
+
     it("should throw connection is refused, when database is unreachable", async () => {
+        let err;
         try {
             await runMigration(1, "up");
-        } catch (err) {
+        } catch (e) {
+            err = e;
+        } finally {
+            expect((err as Error).message).not.toBeUndefined();
             expect((err as Error).message).toContain("ECONNREFUSED");
         }
     });
 
     it("should throw, when projectID not found", async () => {
         const projectID = 99;
+        let err;
         try {
             await runMigration(projectID, "up");
-        } catch (err) {
+        } catch (e) {
+            err = e;
+        } finally {
+            expect((err as Error).message).not.toBeUndefined();
             expect((err as Error).message).toBe(
                 `Project with the ID ${projectID} doesn't exist!`
             );
