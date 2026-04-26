18bec3f8bd62e087884856032ab2f9e409e0013f
Márton Spiteller <spitellermarton@gmail.com>
2026-04-26 16:59:31 +0200

    test: extend colors and connector helper tests, remove messy runner integration test

--- src/tests/unit/internals/connector.test.ts
+++ src/tests/unit/internals/connector.test.ts
@@ -1,5 +1,6 @@
 import { PostgresConnector } from "../../../internals/connectors/postgres";
 import { getConnection } from "../../../internals/connectors/helper";
+import { SqliteConnector } from "../../../internals/connectors/sqlite";
 
 describe("getConnection", () => {
     it("should throw, when invalid protocol passed in connection string", () => {
@@ -12,9 +13,19 @@ describe("getConnection", () => {
         expect(() => getConnection("")).toThrow("Invalid connection string!");
     });
 
-    it("should return an instance with getClient and disconnect", () => {
+    it("should return an postgres instance with getClient and disconnect", () => {
         const connection = getConnection("postgresql://some_pg_database");
         expect(connection).toHaveProperty("getClient");
         expect(connection).toHaveProperty("disconnect");
+        expect(connection).toBeInstanceOf(PostgresConnector);
+    });
+
+    it("should return an sqlite instance with getClient and disconnect", () => {
+        const connection = getConnection(
+            "sqlite://some_sqlite_database.temp.db"
+        );
+        expect(connection).toHaveProperty("getClient");
+        expect(connection).toHaveProperty("disconnect");
+        expect(connection).toBeInstanceOf(SqliteConnector);
     });
 });
